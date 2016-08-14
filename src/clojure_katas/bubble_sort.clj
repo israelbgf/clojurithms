f0f941a7a3c1258be736428fe9eace93a1179430
Israel Fonseca <israel.bgf@gmail.com>
2016-08-14 13:25:28 -0300

    Better indentation for the cond statement

--- src/clojure_katas/bubble_sort.clj
+++ src/clojure_katas/bubble_sort.clj
@@ -4,11 +4,12 @@
 (defn to-tail-if-greater
   [coll val]
   (cond
-    (empty? coll) (list val)
+    (empty? coll)
+      (list val)
     (> (last coll) val)
-    (concat (butlast coll) [val] [(last coll)])
+      (concat (butlast coll) [val] [(last coll)])
     :else
-    (concat coll [val])))
+      (concat coll [val])))
 
 (defn bubble-up [coll]
   (reduce to-tail-if-greater [] coll))
